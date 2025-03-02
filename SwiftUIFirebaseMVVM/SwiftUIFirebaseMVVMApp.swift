1f15176c8904b596e0ea8f12c96585e862ff6567
arhaaan <85577285+arhaaan@users.noreply.github.com>
2025-03-02 14:25:01 +0700

    Add files via upload

---
+++ SwiftUIFirebaseMVVM/SwiftUIFirebaseMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseMVVMApp.swift
+//  SwiftUIFirebaseMVVM
+//
+//  Created by Karim Arhan on 02/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
